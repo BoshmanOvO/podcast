e0c37ebd4bb65f04714a767ceaa17873df282f3e
BoshmanOvO <kumarashishbostan@gmail.com>
2024-06-04 15:06:21 +0530

    Convex database connected well and with some test data, will add some real data in the next one

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,28 +1,35 @@
-import React from 'react';
+"use client";
+import React from "react";
 import PodcastCards from "@/components/PodcastCards";
-import {podcastData} from "@/constants/podcastData";
+import { podcastData } from "@/constants/podcastData";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 const Home = () => {
-    return (
-        <div className={"mt-9 flex flex-col gap-9"}>
-            <section className={'flex flex-col gap-5'}>
-                <h1 className={'text-4xl font-bold text-white-1'}>
-                    Trending Podcast
-                    <div className={'podcast_grid'}>
-                        {podcastData.map(({id, title, description, imgURL}) => (
-                            <PodcastCards
-                                key={id}
-                                imageURL={imgURL}
-                                description={description}
-                                title={title}
-                                podcastID={id}
-                            />
-                        ))}
-                    </div>
-                </h1>
-            </section>
-        </div>
-    );
+  const tasks = useQuery(api.tasks.get);
+  return (
+    <div className={"mt-9 flex flex-col gap-9"}>
+      <section className={"flex flex-col gap-5"}>
+        <h1 className={"text-4xl font-bold text-white-1"}>
+          Trending Podcast
+          <div className="flex min-h-screen flex-col items-center justify-between p-24">
+            {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
+          </div>
+          <div className={"podcast_grid"}>
+            {podcastData.map(({ id, title, description, imgURL }) => (
+              <PodcastCards
+                key={id}
+                imageURL={imgURL}
+                description={description}
+                title={title}
+                podcastID={id}
+              />
+            ))}
+          </div>
+        </h1>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type {Metadata} from "next";
 import {Inter} from "next/font/google";
 import "./globals.css";
 import {icons} from "lucide-react";
+import {ConvexProvider} from "convex/react";
+import ConvexClientProvider from "@/app/ConvexClientProvider";
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -20,7 +22,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+        <ConvexClientProvider>{children}</ConvexClientProvider>
+        </body>
         </html>
     );
 }
